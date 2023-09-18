1cf878a6e564749a31bf25e946de6efc75d4b01a
NullSploit01 <harshaldharmik099@gmail.com>
2023-09-18 21:52:42 +0530

    add request validations

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,30 @@
 import express from 'express'
+import { body } from 'express-validator'
 
 import { authController } from '../controllers/auth.controller'
+import { requestValidator } from '../middlewares/request-validator.middleware'
 
 const router = express.Router()
 
-router.post('/login', authController.login)
+router.post(
+  '/login',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('Please provide a password')
+  ],
+  requestValidator,
+  authController.login
+)
 
-router.post('/register', authController.register)
+router.post(
+  '/register',
+  [
+    body('name').trim().notEmpty().withMessage('Please provide a Name'),
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('Please provide a password')
+  ],
+  requestValidator,
+  authController.register
+)
 
 export { router as authRouter }
